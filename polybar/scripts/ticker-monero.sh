6051ea86e0103c300da7dc6602380778445eb6b6
MakisChristou <Makis_Christou@protonmail.com>
2021-01-05 15:27:29 +0200

    :(

---
+++ polybar/scripts/ticker-monero.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+API="https://api.kraken.com/0/public/Ticker"
+
+quote=$(curl -sf $API?pair=XMRUSD | jq -r ".result.XXMRZUSD.c[0]")
+quote=$(LANG=C printf "%.2f" "$quote")
+
+echo "$quote $"
