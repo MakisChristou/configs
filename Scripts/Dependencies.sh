a28a564eaf4d03eb2a2d23192ea36152a9e2ab41
MakisChristou <makis_christou@protonmail.com>
2022-04-09 20:05:03 +0300

    Minor Changes

--- Scripts/Dependencies.sh
+++ Scripts/Dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install i3 suckless-tools i3lock i3status vim tmux htop ranger compton xfce4-power-manager feh
+sudo apt install i3 suckless-tools i3lock i3status vim tmux htop ranger compton xfce4-power-manager feh lxappearance arandr keepassxc
--- Scripts/Get_Vpns.sh
+++
@@ -1,3 +0,0 @@
-scp -i ~/.ssh/1080ti pi@$GRANDMA_IP:~/ovpns/* .
-scp -i ~/.ssh/1080ti pi@$NICOSIA_IP:~/ovpns/* .
-scp -P 314 -i ~/.ssh/1080ti makis@$HOME_IP:~/ovpns/* .
